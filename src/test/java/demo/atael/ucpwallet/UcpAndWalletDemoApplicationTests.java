a20e4682b9b5679bfb22e4afcf83a59f12037219
Andy Tael <andy.tael@yahoo.com>
2024-10-03 17:13:33 -0500

    Initial commit

---
+++ src/test/java/demo/atael/ucpwallet/UcpAndWalletDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.atael.ucpwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UcpAndWalletDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
